18ed419936aee5cfd9ebcbd0a9938f74bc8d0c46
Ju-1204 <judithmayer8@gmail.com>
2026-05-23 04:05:32 +0200

    neues Passwort erstellen -test

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import secrets
 import string
+import bcrypt
+import json
+import os
 
 app = Flask(__name__)
+STORE_FILE = "pw_speichern.json"
+
+def load_passwords():
+    if not os.path.exists(STORE_FILE):
+        return {}
+
+    with open(STORE_FILE, "r") as file:
+        return json.load(file)
+
+def save_passwords(data):
+    with open(STORE_FILE, "w") as file:
+        json.dump(data, file, indent=4)
 
 def generate_password(length=10):
     alphabet = string.ascii_letters + string.digits + "!§$%&/?#"
     return ''.join(secrets.choice(alphabet) for _ in range(length))
+
+@app.route("/add_pw", methods=["POST"])
+
+def add_pw():
+    data = request.get_json()
+
+    user_id = data.get("user_id")
+    clear_pw = data.get("clear_pw")
+
+    hashed_pw = bcrypt.hashpw(
+        clear_pw.encode("utf-8"),
+        bcrypt.gensalt()
+    ).decode("utf-8")
+
+    passwords = load_passwords()
+
+    passwords[user_id] = hashed_pw
+
+    save_passwords(passwords)
+
+    return jsonify({
+        "message": "Passwort gespeichert"
+    })
 
 @app.route("/generate_pw", methods=["GET"])
 def generate_pw():
